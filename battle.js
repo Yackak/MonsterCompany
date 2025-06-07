af78e7d3343aa7a29cd1caa68bb285fbfa9e01d8
Yackak <97081932+Yackak@users.noreply.github.com>
2025-06-07 22:04:41 +0900

    Update battle.js

--- battle.js
+++ battle.js
@@ -5,7 +5,7 @@ class BattleScene extends Phaser.Scene {
 
   preload() {
     // 배틀에 필요한 이미지나 리소스 로드
-    this.load.image('battle_bg', 'assets/battle_bg.png');
+    this.load.image('background_battle', 'assets/background_battle.png');
     // 캐릭터, 이펙트 등도 여기서 로드
   }
 
@@ -13,6 +13,10 @@ class BattleScene extends Phaser.Scene {
     // 배경 띄우기
     this.add.image(0, 0, 'background_battle').setOrigin(0, 0);
 
+
+    // 디버깅용
+    console.log("21:56");
+    
     // 예시: 텍스트로 배틀 씬 표시
     this.add.text(400, 300, 'BATTLE START!', {
       fontSize: '48px',
